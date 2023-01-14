890589295630d1f277efbbebe2dc9c4d2aec9525
Will <80670903+Hitchhiker007@users.noreply.github.com>
2023-01-15 08:17:19 +1100

    Delete index.js
    script put into index.html

--- index.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementsByClassName('menu-icon')[0]
-const navbar = document.getElementsByClassName('nav-bar')[0]
-
-menu.addEventListener('click', () => {
-    navbar.classList.toggle('active')
-})
